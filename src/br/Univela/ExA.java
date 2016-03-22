4a6c0f9963bf96c0aa94b4808a053c771961dff9
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-03-21 23:20:59 -0300

    ExA

---
+++ src/br/Univela/ExA.java
@@ -0,0 +1,24 @@
+package br.Univela;
+
+
+public class ExA implements Desenho{
+
+	@Override
+	public void desenhar() {
+		System.out.println("A)\n\n");
+		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if(i>=j){
+					System.out.print("*");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+	
+}
+
