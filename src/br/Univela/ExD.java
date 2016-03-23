5ef8edcf93c2c1d0fd455ac27677fac7ff574fac
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-03-22 21:27:17 -0300

    Commit ExD

---
+++ src/br/Univela/ExD.java
@@ -0,0 +1,27 @@
+package br.Univela;
+
+
+public class ExD implements Desenho{
+	
+	public static int SIZE = 9;
+	
+	@Override
+	public void desenhar() {
+		
+		System.out.println("D)\n\n"); 
+		
+		int brancos = 9;
+		for (int linha = 0; linha < SIZE; linha++) {
+			for (int coluna = 0; coluna < SIZE; coluna++) {
+				if(coluna<brancos-1){
+					System.out.print(" "); 
+				}else{
+					System.out.print("*");  
+				}
+			}
+			brancos--;
+			System.out.println();
+		}
+	}
+
+}
