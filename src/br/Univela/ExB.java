5ef5958cbd9232da1c308bbb560b261ccb62e606
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-03-22 21:26:32 -0300

    Commit ExB

--- src/br/Univela/ExB.java
+++ src/br/Univela/ExB.java
@@ -7,7 +7,7 @@ public class ExB implements Desenho{
 	public void desenhar() {
 		System.out.println("B)\n\n");
 		
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) { 
 			for (int j = 0; j < 10; j++) {
 				if(i<=j){
 					System.out.print("*");
